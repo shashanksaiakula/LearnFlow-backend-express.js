22bb7b23c2f747d589b86bdccfa177470cb342d0
shashanksaiakula <shashanksaiakula@gmail.com>
2026-08-25 20:21:39 +0530

    added port 8000 gor gcp to develop

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const homeRouter = require("./routes/home");
 const courseRouter = require("./routes/courses");
